ff92478fa35b5216e1ffa3d414d92adf79d4ee2a
ichoi21 <ihchoi@gmail.com>
2020-06-01 20:55:27 -0700

    update with more functions

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,4 @@
-const { add, minus } = require("../util/util");
+const { add, minus, multiply, divide, power } = require("../util/util");
 
 describe("Add", () => {
   describe("Success", () => {
@@ -35,11 +35,77 @@ describe("Minus", () => {
     });
 
     it("Should throw an error if the second argument is not a number", () => {
-      expect(() => minus(2, "4")).toThrowError();
+      expect(() => minus(2, "")).toThrowError();
     });
 
     it("Should throw an error if no arguments were received", () => {
       expect(() => minus()).toThrowError();
+    });
+  });
+});
+
+describe("Mulitply", () => {
+  describe("Success", () => {
+    it("Should return the sum of the 2 passed arguments", () => {
+      expect(multiply(2, 2)).toBe(4);
+    });
+  });
+
+  describe("Fail", () => {
+    it("Should throw an error if the first argument is not a number", () => {
+      expect(() => multiply("2", 2)).toThrowError();
+    });
+
+    it("Should throw an error if the second argument is not a number", () => {
+      expect(() => multiply(2, "2")).toThrowError();
+    });
+
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => multiply()).toThrowError();
+    });
+  });
+});
+
+describe("Divide", () => {
+  describe("Success", () => {
+    it("Should return the sum of the 2 passed arguments", () => {
+      expect(divide(2, 4)).toBe(0.6);
+    });
+  });
+
+  describe("Fail", () => {
+    it("Should throw an error if the first argument is not a number", () => {
+      expect(() => divide("4", 2)).toThrowError();
+    });
+
+    it("Should throw an error if the second argument is not a number", () => {
+      expect(() => divide(4, "2")).toThrowError();
+    });
+
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => divide()).toThrowError();
+    });
+  });
+});
+
+describe("Power", () => {
+  describe("Success", () => {
+    it("Should return the sum of the 2 passed arguments", () => {
+      expect(power(4, 4)).toBe(256);
+    });
+  });
+
+  describe("Fail", () => {
+    it("Should throw an error if the first argument is not a number", () => {
+      expect(() => power("4", 2)).toThrowError();
+    });
+
+    it("Should throw an error if the second argument is not a number", () => {
+      expect(() => power(4, "2")).toThrowError();
+    });
+
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => power()).toThrowError();
     });
   });
 });
--- util/util.js
+++ util/util.js
@@ -14,4 +14,28 @@ const minus = (num1, num2) => {
   return num1 - num2;
 };
 
-module.exports = { add, minus };
+const multiply = (num1, num2) => {
+  if (typeof num1 != "number")
+    throw new Error("First Argument must be a number");
+  if (typeof num2 != "number")
+    throw new Error("Second Argument must be a number");
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  if (typeof num1 != "number")
+    throw new Error("First Argument must be a number");
+  if (typeof num2 != "number")
+    throw new Error("Second Argument must be a number");
+  return num1 / num2;
+};
+
+const power = (num1, num2) => {
+  if (typeof num1 != "number")
+    throw new Error("First Argument must be a number");
+  if (typeof num2 != "number")
+    throw new Error("Second Argument must be a number");
+  return Math.pow(num1, num2);
+};
+
+module.exports = { add, minus, multiply, divide, power };
