8292d329d8e4b66337a9c75f002778a3e0df7af1
ichoi21 <ihchoi@gmail.com>
2020-06-01 19:27:28 -0700

    added add and minus

--- test/util.test.js
+++ test/util.test.js
@@ -6,4 +6,16 @@ describe("Add", () => {
       expect(add(2, 2)).toBe(4);
     });
   });
+
+  describe("Fail", () => {
+    it("Should throw an error if the first argument is not a number", () => {
+      expect(() => add("2", 2)).toThrowError();
+    });
+
+    it("Should throw an error if the second argument is not a number", () => {
+      expect(() => add(2, "2")).toThrowError();
+    });
+  });
 });
+
+describe("Minus", () => {});
--- util/util.js
+++ util/util.js
@@ -1,5 +1,11 @@
 const add = (num1, num2) => {
+  if (typeof num1 != "number")
+    throw new Error("First Argument must be a number");
+  if (typeof num2 != "number")
+    throw new Error("Second Argument must be a number");
   return num1 + num2;
 };
 
-module.exports = { add };
+const minus = (num1, num2) => {};
+
+module.exports = { add, minus };
